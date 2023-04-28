0d644570edd5c0841669acdc13a196d4233b5324
Hiếu Nguyễn Đức <ndhieuvegia983@gmail.com>
2023-04-28 11:17:12 +0700

    remove URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: 3000,
-    MONGO_URI: "mongodb+srv://ndhieuvegia983:Hieu0976930989%40@cluster0.bxocrwj.mongodb.net/chat?retryWrites=true&w=majority"
+    MONGO_URI: "mongodb+srv://ndhieuvegia983:<password>@cluster0.bxocrwj.mongodb.net/chat?retryWrites=true&w=majority"
 }
