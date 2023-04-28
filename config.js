7b0f2f652d0c1f331e3c55da3fdfd48617b6924b
Hiếu Nguyễn Đức <ndhieuvegia983@gmail.com>
2023-04-28 11:09:17 +0700

    connect mongoDB

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT: 3000,
+    MONGO_URI: "mongodb+srv://ndhieuvegia983:Hieu0976930989%40@cluster0.bxocrwj.mongodb.net/chat?retryWrites=true&w=majority"
+}
--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io')
 const io = new Server(server)
+const CONFIG = require('./config')
+const mongoose = require('mongoose')
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+/**Socket IO */
 io.on('connection', (socket) => {
     console.log('a user connected')
     if (socket.on('disconnect', () => {
@@ -20,8 +23,13 @@ io.on('connection', (socket) => {
     })
 })
 
+/**Connect mongoDB */
+mongoose.connect(CONFIG.MONGO_URI, { maxPoolSize: 10 }).then(() => {
+    console.log("Connected mongoDb");
+}).catch(err => {
+    console.log(`Connect mongoDB fail: ${err}`);
+})
 
-
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(CONFIG.PORT, () => {
+    console.log(`listening on *:${CONFIG.PORT}`);
 });
